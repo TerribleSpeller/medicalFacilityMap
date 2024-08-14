65d4c3d35c2a316229d6c67bb7309f4e5d725993
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2024-08-14 16:23:55 +0700

    FirstForaysintoPromos

---
+++ public/js/detailsPromos.js
@@ -0,0 +1,11 @@
+const detailsPromos = [
+    {
+        tenantName: "RS Eka Hospital",
+        promoName: "Festival Sehat Merdeka Bersama Eka Hospital",
+        promoPrice: "Rp 1.7 Juta",
+        promoDesc: "Saatnya merdeka dari penyakit, ayo periksa kesehatanmu sekarang dengan penawaran spesial. Promo ini berlaku hanya sampai 31 Agustus 2024 ya Sobat Sehat jangan lupa di share ke grup keluarga.",
+        promoPic: "https://www.ekahospital.com/storage/package/1723436398-WhatsApp-Image-2024-08-12-at-11.11.14.jpeg"
+    }
+]
+  
+export default detailsPromos;
